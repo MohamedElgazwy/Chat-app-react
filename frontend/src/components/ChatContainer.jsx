3ff983af96e3319dfb178e29fc2203b5dcc514c0
Mohamed Algazwy <almyhwbmhmd26@gmail.com>
2025-11-10 20:01:21 +0200

    modify chatcontainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const ChatContainer = () => {
   return (
     <div>
-      <h1>chatcontainer</h1>
+      <div className="container-message text-2xl font-semibold mb-4 bg-gray-50 p-4 rounded-lg shadow-sm">
+        <div className="message">
+          <p>This is a chat container.</p>
+        </div>
+      </div>
     </div>
   );
 };
